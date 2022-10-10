48f2d06c951fbe277dad70837054d4999644fa03
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-10-10 16:01:40 +0530

    Initial Commit

---
+++ OfficeMeetingApp/OfficeMeetingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OfficeMeetingAppApp.swift
+//  OfficeMeetingApp
+//
+//  Created by Bhumika Patel on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct OfficeMeetingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
