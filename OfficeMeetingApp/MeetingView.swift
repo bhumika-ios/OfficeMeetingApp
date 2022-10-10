45be295d656ccb15faf2eb13e7257536670331b1
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-10-10 16:19:42 +0530

    create text

--- OfficeMeetingApp/MeetingView.swift
+++ OfficeMeetingApp/MeetingView.swift
@@ -11,6 +11,10 @@ struct MeetingView: View {
     var body: some View {
         VStack {
            ProgressView(value: 5,total: 15)
+            HStack{
+                Text("Seconds Elapsed")
+                Text("Seconds Remaining")
+            }
         }
         .padding()
     }
